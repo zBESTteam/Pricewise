6765a70e2dc4e4caadaf9c37574fd9a8cff06982
AlexeiYM <alyakushevich.2006@gmail.com>
2025-12-14 23:26:38 +0300

    optimized imports

--- app/src/main/java/com/vkedu/pricewise/MainActivity.kt
+++ app/src/main/java/com/vkedu/pricewise/MainActivity.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
