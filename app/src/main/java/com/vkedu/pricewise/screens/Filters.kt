52798e3401271da45877d4682e5cbe320d49e627
AlexeiYM <alyakushevich.2006@gmail.com>
2025-12-16 00:03:28 +0300

    switches added

--- app/src/main/java/com/vkedu/pricewise/screens/Filters.kt
+++ app/src/main/java/com/vkedu/pricewise/screens/Filters.kt
@@ -1,5 +1,6 @@
 package com.vkedu.pricewise.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -28,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -42,6 +45,11 @@ import com.vkedu.pricewise.R
 fun Filters(sheetState: SheetState, closeFilters: () -> Unit) {
     var isProductChosen by rememberSaveable { mutableStateOf(true) }
     var deliveryChosen by rememberSaveable { mutableStateOf(0) }
+    var onlyOriginals by rememberSaveable { mutableStateOf(false) }
+    var onlyNew by rememberSaveable { mutableStateOf(false) }
+    var onlyBU by rememberSaveable { mutableStateOf(false) }
+    var onlyMarketplaces by rememberSaveable { mutableStateOf(false) }
+    var onlyOfflineShops by rememberSaveable { mutableStateOf(false) }
     val customModifier: Modifier = Modifier.offset(y = (-7).dp)
     ModalBottomSheet(
         sheetState = sheetState, onDismissRequest = { closeFilters() }, dragHandle =
@@ -160,7 +168,7 @@ fun Filters(sheetState: SheetState, closeFilters: () -> Unit) {
                     }
                 }
                 Text(
-                    text = stringResource(R.string.sort_by_price),
+                    text = stringResource(R.string.delieveryDate),
                     style = TextStyle(
                         fontSize = 16.sp,
                         lineHeight = 24.sp,
@@ -176,114 +184,232 @@ fun Filters(sheetState: SheetState, closeFilters: () -> Unit) {
                     modifier = Modifier
                         .horizontalScroll(rememberScrollState())
                 ) {
-                    Box(
+                    DeliveryDateButton(
+                        text = stringResource(R.string.today),
+                        isSelected = deliveryChosen == 1,
+                        onClick = { deliveryChosen = if (deliveryChosen == 1) 0 else 1 }
+                    )
+                    DeliveryDateButton(
+                        text = stringResource(R.string.today_or_tomorrow),
+                        isSelected = deliveryChosen == 2,
+                        onClick = { deliveryChosen = if (deliveryChosen == 2) 0 else 2 }
+                    )
+                    DeliveryDateButton(
+                        text = stringResource(R.string.week),
+                        isSelected = deliveryChosen == 3,
+                        onClick = { deliveryChosen = if (deliveryChosen == 3) 0 else 3 }
+                    )
+                    DeliveryDateButton(
+                        text = stringResource(R.string.two_weeks),
+                        isSelected = deliveryChosen == 4,
+                        onClick = { deliveryChosen = if (deliveryChosen == 4) 0 else 4 }
+                    )
+                }
+                Text(
+                    text = "Качество товара",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        lineHeight = 24.sp,
+                        fontFamily = FontFamily(Font(R.font.inter)),
+                        fontWeight = FontWeight(600),
+                        color = colorResource(R.color.mid_dark),
+
+                        letterSpacing = 0.3.sp,
+                    )
+                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(13.dp, Alignment.Top),
+                    horizontalAlignment = Alignment.Start,
+                ) {
+                    Row(
                         modifier = Modifier
-                            .height(41.dp)
-                            .background(
-                                color = if (deliveryChosen != 1) colorResource(R.color.disabled_filter_button_color) else colorResource(
-                                    R.color.mid_dark
-                                ),
-                                shape = RoundedCornerShape(size = 14.dp)
-                            )
-                            .clickable { deliveryChosen = if (deliveryChosen == 1) 0 else 1 },
-                        contentAlignment = Alignment.Center
+                            .fillMaxWidth()
+                            .height(44.dp)
+                            .background(color = colorResource(R.color.disabled_filter_button_color), shape = RoundedCornerShape(size = 14.dp))
+                            .padding(start = 14.dp, top = 10.dp, end = 14.dp, bottom = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
                     ) {
                         Text(
-                            modifier = Modifier.padding(all = 10.dp),
-                            text = stringResource(R.string.today),
+                            text = "Только оригиналы",
                             style = TextStyle(
-                                fontSize = 15.sp,
-                                lineHeight = 21.sp,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
                                 fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = if (deliveryChosen != 1) colorResource(R.color.disabled_filter_button_text_color) else colorResource(
-                                    R.color.white
-                                ),
+                                fontWeight = FontWeight(500),
+                                color = colorResource(R.color.disabled_filter_button_text_color),
+
                                 letterSpacing = 0.3.sp,
                             )
                         )
+                        Image(
+                            modifier = Modifier.clickable { onlyOriginals = !onlyOriginals },
+                            painter = if (onlyOriginals) painterResource(R.drawable.switch_on) else painterResource(R.drawable.switch_off),
+                            contentDescription = null
+                        )
                     }
-                    Box(
+                    Row(
                         modifier = Modifier
-                            .height(41.dp)
-                            .background(
-                                color = if (deliveryChosen != 2) colorResource(R.color.disabled_filter_button_color) else colorResource(
-                                    R.color.mid_dark
-                                ),
-                                shape = RoundedCornerShape(size = 14.dp)
-                            )
-                            .clickable { deliveryChosen = if (deliveryChosen == 2) 0 else 2 },
-                        contentAlignment = Alignment.Center
+                            .fillMaxWidth()
+                            .height(44.dp)
+                            .background(color = colorResource(R.color.disabled_filter_button_color), shape = RoundedCornerShape(size = 14.dp))
+                            .padding(start = 14.dp, top = 10.dp, end = 14.dp, bottom = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
                     ) {
                         Text(
-                            modifier = Modifier.padding(all = 10.dp),
-                            text = stringResource(R.string.today_or_tomorrow),
+                            text = "Только новые",
                             style = TextStyle(
-                                fontSize = 15.sp,
-                                lineHeight = 21.sp,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
                                 fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = if (deliveryChosen != 2) colorResource(R.color.disabled_filter_button_text_color) else colorResource(
-                                    R.color.white
-                                ),
+                                fontWeight = FontWeight(500),
+                                color = colorResource(R.color.disabled_filter_button_text_color),
+
                                 letterSpacing = 0.3.sp,
                             )
                         )
+                        Image(
+                            modifier = Modifier.clickable { onlyNew = !onlyNew },
+                            painter = if (onlyNew) painterResource(R.drawable.switch_on) else painterResource(R.drawable.switch_off),
+                            contentDescription = null
+                        )
                     }
-                    Box(
+                    Row(
                         modifier = Modifier
-                            .background(
-                                color = if (deliveryChosen != 3) colorResource(R.color.disabled_filter_button_color) else colorResource(
-                                    R.color.mid_dark
-                                ),
-                                shape = RoundedCornerShape(size = 14.dp)
-                            )
-                            .clickable { deliveryChosen = if (deliveryChosen == 3) 0 else 3 },
-                        contentAlignment = Alignment.Center
+                            .fillMaxWidth()
+                            .height(44.dp)
+                            .background(color = colorResource(R.color.disabled_filter_button_color), shape = RoundedCornerShape(size = 14.dp))
+                            .padding(start = 14.dp, top = 10.dp, end = 14.dp, bottom = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
                     ) {
                         Text(
-                            modifier = Modifier.padding(all = 10.dp),
-                            text = stringResource(R.string.week),
+                            text = "Только Б/У",
                             style = TextStyle(
-                                fontSize = 15.sp,
-                                lineHeight = 21.sp,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
                                 fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = if (deliveryChosen != 3) colorResource(R.color.disabled_filter_button_text_color) else colorResource(
-                                    R.color.white
-                                ),
+                                fontWeight = FontWeight(500),
+                                color = colorResource(R.color.disabled_filter_button_text_color),
+
                                 letterSpacing = 0.3.sp,
                             )
                         )
+                        Image(
+                            modifier = Modifier.clickable { onlyBU = !onlyBU },
+                            painter = if (onlyBU) painterResource(R.drawable.switch_on) else painterResource(R.drawable.switch_off),
+                            contentDescription = null
+                        )
                     }
-                    Box(
+                }
+                Text(
+                    text = "Магазины",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        lineHeight = 24.sp,
+                        fontFamily = FontFamily(Font(R.font.inter)),
+                        fontWeight = FontWeight(600),
+                        color = colorResource(R.color.mid_dark),
+
+                        letterSpacing = 0.3.sp,
+                    )
+                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(13.dp, Alignment.Top),
+                    horizontalAlignment = Alignment.Start,
+                ) {
+                    Row(
                         modifier = Modifier
-                            .background(
-                                color = if (deliveryChosen != 4) colorResource(R.color.disabled_filter_button_color) else colorResource(
-                                    R.color.mid_dark
-                                ),
-                                shape = RoundedCornerShape(size = 14.dp)
-                            )
-                            .clickable { deliveryChosen = if (deliveryChosen == 4) 0 else 4 },
-                        contentAlignment = Alignment.Center
+                            .fillMaxWidth()
+                            .height(44.dp)
+                            .background(color = colorResource(R.color.disabled_filter_button_color), shape = RoundedCornerShape(size = 14.dp))
+                            .padding(start = 14.dp, top = 10.dp, end = 14.dp, bottom = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
                     ) {
                         Text(
-                            modifier = Modifier.padding(all = 10.dp),
-                            text = stringResource(R.string.two_weeks),
+                            text = "Маркетплейсы",
                             style = TextStyle(
-                                fontSize = 15.sp,
-                                lineHeight = 21.sp,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
                                 fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = if (deliveryChosen != 4) colorResource(R.color.disabled_filter_button_text_color) else colorResource(
-                                    R.color.white
-                                ),
+                                fontWeight = FontWeight(500),
+                                color = colorResource(R.color.disabled_filter_button_text_color),
+
                                 letterSpacing = 0.3.sp,
                             )
+                        )
+                        Image(
+                            modifier = Modifier.clickable { onlyMarketplaces = !onlyMarketplaces },
+                            painter = if (onlyMarketplaces) painterResource(R.drawable.switch_on) else painterResource(R.drawable.switch_off),
+                            contentDescription = null
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(44.dp)
+                            .background(color = colorResource(R.color.disabled_filter_button_color), shape = RoundedCornerShape(size = 14.dp))
+                            .padding(start = 14.dp, top = 10.dp, end = 14.dp, bottom = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text(
+                            text = "Оффлайн магазины",
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                fontFamily = FontFamily(Font(R.font.inter)),
+                                fontWeight = FontWeight(500),
+                                color = colorResource(R.color.disabled_filter_button_text_color),
+
+                                letterSpacing = 0.3.sp,
+                            )
+                        )
+                        Image(
+                            modifier = Modifier.clickable { onlyOfflineShops = !onlyOfflineShops },
+                            painter = if (onlyOfflineShops) painterResource(R.drawable.switch_on) else painterResource(R.drawable.switch_off),
+                            contentDescription = null
                         )
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun DeliveryDateButton(
+    text: String,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .height(41.dp)
+            .background(
+                color = if (!isSelected) colorResource(R.color.disabled_filter_button_color) else colorResource(
+                    R.color.mid_dark
+                ),
+                shape = RoundedCornerShape(size = 14.dp)
+            )
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier.padding(all = 10.dp),
+            text = text,
+            style = TextStyle(
+                fontSize = 15.sp,
+                lineHeight = 21.sp,
+                fontFamily = FontFamily(Font(R.font.inter)),
+                fontWeight = FontWeight(600),
+                color = if (!isSelected) colorResource(R.color.disabled_filter_button_text_color) else colorResource(
+                    R.color.white
+                ),
+                letterSpacing = 0.3.sp,
+            )
+        )
     }
 }
