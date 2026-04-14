5cee11818f7113c32ba4477f078da6b3ccecafe1
Kraft410-a <arina.kraft11@mail.ru>
2026-04-14 16:16:41 +0300

    change 2

--- feature/profile/impl/build.gradle.kts
+++ feature/profile/impl/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+/* plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
@@ -21,4 +21,46 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     ksp(libs.room.compiler)
+} */
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.pricewise.feature.profile.impl"
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(project(":feature:profile:api"))
+    
+
+    implementation(project(":core:navigation-api"))
+    implementation(project(":core:di"))
+    implementation(project(":core:ui"))
+    
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
